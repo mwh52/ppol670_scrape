735c222b855d34fc65f1f5137bb0893b825da1dc
Huade <hh561@georgetown.edu>
2015-02-17 17:07:24 -0500

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# ppol670_scrape
-PPOL 670 Programming Assignment - Scraping
+# PPOL 670 Programming Assignment - Scraping
+
+This analysis use data from the [NY Times API](http://developer.nytimes.com/docs/read/article_search_api_v2) to create a monthly time series of coverage of budget deficits issues.
+
+Budget Deficit Coverage in NYTimes (all time)
+![Budget Deficit Coverage in NYTimes (all time)](figure/all_time.png)
+
+Budget Deficit Coverage in NYTimes (2000 - current)
+![Budget Deficit Coverage in NYTimes (2000-now)](figure/since_2000.png)
+
+In this repo:
+
+-  `nytimes.py` is the Python script to retrive information with NY Times Article Search API v2. 12 key words were used in the query (see details later). Scraped data was inserted into ScraperWiki's data table with SQL queries and downloaded from ScraperWiki. The code is scheduled to run hourly on ScraperWiki while keeping the progress (since ScraperWiki automatically terminate task hourly).  This code was deployed on [ScraperWiki](https://scraperwiki.com/).
+-  `nytimes.csv` is the collected information from NY Times Article Search API v2.
+-  `visualization.R` is the R script to clean the collected information and visualize it. Duplicated articles were removed based on article id. 
+-  `figures` folder contains final graph outputs.
+
+The keywords used in this analysis include:
+    budget deficit, compensatory spending, debt, government debt, debt explosion, deficit financing, in the red, megadebt, negative cash flow, no assets, overspending, national debt
--- nytimes.py
+++ nytimes.py
@@ -6,61 +6,77 @@ import json
 import time
 
 # Get total pages
+# Note: Void API Key for demo only.
+
 def get_pages(key_word):
-    payload = {'api-key':'fa45552479ff69987f5809b3f911bc1c:9:71252208', 
-               'q':key_word}
-    r = requests.get('http://api.nytimes.com/svc/search/v2/articlesearch.json?', params = payload)
+    payload = {'api-key': 'fa45552479ff69987f5809b3f911bc1c:9:71252208',
+               'q': key_word}
+    r = requests.get(
+        'http://api.nytimes.com/svc/search/v2/articlesearch.json?',
+        params=payload)
     response = json.loads(r.text)['response']['meta']['hits']
-    if int(response/10)-1 < 100:
-        return int(response/10)-1
+    if int(response / 10) - 1 < 100:
+        return int(response / 10) - 1
     else:
         return 100
 
 # Request from New York Times
+
+
 def api_request(key_word, page=1):
     time.sleep(10)
-    payload = {'api-key':'fa45552479ff69987f5809b3f911bc1c:9:71252208', 
-               'q':key_word,
-               'page':page}
-    r = requests.get('http://api.nytimes.com/svc/search/v2/articlesearch.json?', params = payload)
+    payload = {'api-key': 'fa45552479ff69987f5809b3f911bc1c:9:71252208',
+               'q': key_word,
+               'page': page}
+    r = requests.get(
+        'http://api.nytimes.com/svc/search/v2/articlesearch.json?',
+        params=payload)
     response = json.loads(r.text)['response']['docs']
     return response
 
 # Append responses to table
+
+
 def append_table(response, keyword, page):
     for article in response:
-        scraperwiki.sqlite.execute("INSERT INTO nytimes (epoch_time, id, date_time, keyword, page) VALUES (?,?,?,?,?)", 
-                                   (int(100*time.time()), article['_id'], article['pub_date'], keyword, page))
+        scraperwiki.sqlite.execute("INSERT INTO nytimes (epoch_time, id, date_time, keyword, page) VALUES (?,?,?,?,?)",
+                                   (int(100 * time.time()), article['_id'], article['pub_date'], keyword, page))
 
 # List of keywords
-keywords = ['budget deficit','compensatory spending','debt','government debt',
-            'debt explosion','deficit financing','in the red','megadebt',
-            'negative cash flow','no assets','overspending','national debt']
+keywords = ['budget deficit', 'compensatory spending', 'debt', 'government debt',
+            'debt explosion', 'deficit financing', 'in the red', 'megadebt',
+            'negative cash flow', 'no assets', 'overspending', 'national debt']
 
 # Continue scrape
+
+
 def left_over():
     try:
-        current = scraperwiki.sqlite.execute("SELECT keyword, page FROM nytimes ORDER BY epoch_time DESC LIMIT 1")
+        current = scraperwiki.sqlite.execute(
+            "SELECT keyword, page FROM nytimes ORDER BY epoch_time DESC LIMIT 1")
         current_word = current['data'][0][0]
         current_page = current['data'][0][1]
-        return [current_word,current_page]
+        return [current_word, current_page]
     except:
-        scraperwiki.sqlite.execute("CREATE TABLE nytimes (epoch_time int, id string, date_time string, keyword string, page int)")
+        scraperwiki.sqlite.execute(
+            "CREATE TABLE nytimes (epoch_time int, id string, date_time string, keyword string, page int)")
         return ['budget deficit', 0]
 
 # Main function
+
+
 def main():
     current_pos = left_over()
-    keyword_pos = [i for i,x in enumerate(keywords) if x == current_pos[0]][0]
+    keyword_pos = [i for i, x in enumerate(keywords) if x == current_pos[0]][0]
     for i in range(keyword_pos, len(keywords)):
         try:
             page_count = get_pages(keywords[i])
-            
-            if i==keyword_pos:
+
+            if i == keyword_pos:
                 start_page = int(current_pos[1])
             else:
                 start_page = 0
-            for page_num in range(start_page,page_count):
+            for page_num in range(start_page, page_count):
                 try:
                     response_info = api_request(keywords[i], page_num)
                     append_table(response_info, keywords[i], page_num)
@@ -70,7 +86,5 @@ def main():
             pass
 
 
-if __name__ == '__main__' :
+if __name__ == '__main__':
     main()
-
-
